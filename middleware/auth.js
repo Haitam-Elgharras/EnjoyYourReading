eca68c2873cf2e2e49e7cdfef562d5aecafa7e1f
Haitam-Elgharras <elgharras.haitam@gmail.com>
2023-05-23 11:06:13 +0100

    new version2

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,9 @@ function auth(req, res, next) {
   if (!token) return res.status(401).send("access denied no token provided");
   try {
     //if there is a token we need to verify it
-    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    // const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    const decoded = jwt.verify(token, process.env.jwtPrivateKey);
+
     //when we decode the token we get an object that contains information about the user and the private key
     // so we store the decoded object in the req.user to use it in the next middleware
     req.user = decoded;
